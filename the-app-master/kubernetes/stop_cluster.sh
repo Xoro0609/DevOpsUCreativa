cd9c4b389a4984a70741df4344bcde928b14380c
ivanac <xoro0609@outlook.com>
2022-07-01 21:23:47 -0600

    Second commit, first test

--- the-app-master/kubernetes/stop_cluster.sh
+++ the-app-master/kubernetes/stop_cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd vagrant
-NUM_INSTANCES=3 vagrant up
+NUM_INSTANCES=3 vagrant up                                                                                                                                                                                                                  6                                                                
 cd ..
